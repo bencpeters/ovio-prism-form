a722480e38cdc322dcff7c9e2622fbb1fdcab4e8
Ben Peters <bencpeters@gmail.com>
2019-02-11 22:26:03 -0700

    Added Heroku logging

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # Simple Flask wrapper to proxy form submissions into Air Table
 
 import os
+import sys
+import logging
 from functools import wraps
 from urllib.parse import urlparse
 from requests.exceptions import HTTPError
@@ -17,6 +19,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app, resources={"/submit": {"origins": os.getenv("FORM_SERVER")}})
 
+# Heroku logging
+if 'DYNO' in os.environ:
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.ERROR)
+
 
 def filter_by_request_url(f):
     """Crude filter for requests to attempt to ensure that they are coming from a valid host.
@@ -25,7 +32,7 @@ def filter_by_request_url(f):
     @wraps(f)
     def decorated_func(*args, **kwargs):
         host = urlparse(request.environ['HTTP_ORIGIN'])
-        allowed = urlparse(os.getenv("FORM_SERVER")) 
+        allowed = urlparse(os.getenv("FORM_SERVER"))
         if host.hostname != allowed.hostname or host.port != allowed.port:
             return jsonify({"Error": "Not authorized to make this request."}), 401
         else:
@@ -40,7 +47,7 @@ def submit():
         data = transform_form_to_airtable(request.form)
         send_to_airtable(data)
     except HTTPError as err:
-        app.logger.error(f"Error saving to AirTable: {err}\nAttempted Write: {data}") 
+        app.logger.error(f"Error saving to AirTable: {err}\nAttempted Write: {data}")
         raise err
 
     return jsonify({"status": "success"})
@@ -48,6 +55,7 @@ def submit():
 
 @app.errorhandler(Exception)
 def handle_error(e):
+    app.logger.error(f"Error handling request: {e}")
     return jsonify({"Error": "Failed to save form data. Try resubmitting, or email support@oviohub.com if the problem persists."}), 500
 
 
